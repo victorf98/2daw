5724d879d55f493ab944b1626a5e343598233b9b
victorf98 <vflores111@boscdelacoma.cat>
2022-09-20 00:31:42 +0200

    start to decrypt

--- UF1/A1.1_encriptacio_inventada.php
+++ UF1/A1.1_encriptacio_inventada.php
@@ -7,14 +7,53 @@
     <?php
     
         function encriptacio($text, $ip){
+            $text = iconv("UTF-8", "ASCII//TRANSLIT", $text);
             $ip_bin = decbin(sumar_caracters($ip));
-            $sum = 0;
-            while ($ip_bin != 0) {
-                $sum += $ip_bin % 10;
-                $ip_bin = intdiv($ip_bin, 10);
+            $sum = sumar_digits($ip_bin);
+
+            return transformar_text($text, $sum);
+        }
+
+        function decriptacio($text, $ip){
+            $ip_bin = decbin(sumar_caracters($ip));
+            $sum = sumar_digits($ip_bin);
+
+            return desfer_codi($text, $sum);
+        }
+
+        function transformar_text($text, $sum){
+            $final_text = "";
+            foreach (str_split($text) as $char) {
+                //Si el caràcter es minúscula
+                if (ord($char) >= 97 and ord($char) <= 122) {
+                    if (ord(strtoupper($char)) + $sum > ord("Z")) {
+                        $final_text .= chr(ord("A") + (ord(strtoupper($char)) + $sum) - ord(strtoupper($char)));
+                    }else {
+                        $final_text .= chr(ord(strtoupper($char)) + $sum);
+                    }
+                //Si el caràcter es majúscula
+                }elseif (ord($char) >= 65 and ord($char) <= 90) {
+                    $final_text .= strval(ord($char));
+                //Si el caràcter es número
+                }elseif (ord($char) >= 48 and ord($char) <= 57) {
+                    $final_text .= chr(ord("a") + intval($char));
+                //Altres
+                }else {
+                    $final_text .= "sp00" . ord($char);
+                }
             }
 
-            transformar_text($text);
+            return $final_text;
+        }
+
+        function desfer_codi($text, $sum){
+            $text_array = str_split($text);
+            for ($i=0; $i < count($text_array); $i++) { 
+                if ($text_array[i]) {
+                    
+                }
+            } 
+            
         }
 
         function sumar_caracters($text){
@@ -25,22 +64,16 @@
             return $suma;
         }
 
-        function tranformar_text($text){
-            $final_text = "";
-            foreach ($text as $char) {
-                if (IntlChar::isupper($char)) {
-                    $final_text .= chr(ord(strtolower($char)) + $sum);
-                }elseif (IntlChar::islower($char)) {
-                    $final_text .= strval(ord($char))
-                }elseif (is_numeric($char)) {
-                    $final_text .= chr(ord("A") + intval($char));
-                }else {
-                    $final_text .= "sp00" . intval($char);
-                }
+        function sumar_digits($num){
+            $sum = 0;
+            while ($num != 0) {
+                $sum += $num % 10;
+                $num = intdiv($ip_bin, 10);
             }
-
-            return $final_text;
+            return $sum;
         }
+
+        echo encriptacio("Patata_123zz", $_SERVER['REMOTE_ADDR']);
 
     ?>
 </body>
