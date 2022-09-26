d914a9d30a28dbb25a38989e19640308c0611462
victorf98 <vflores111@boscdelacoma.cat>
2022-09-26 17:14:15 +0200

    Exercicis A2 Formularis

---
+++ UF1/A2 Formularis/formulari_3n+1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<title>Formulari 3n + 1</title>
+
+</head>
+
+<body>
+
+<div style="margin: 30px 10%;">
+<h3>3n + 1</h3>
+<form action="processa_3n+1.php" method="post" id="myform" name="myform">
+
+	<label>Numero</label> <input type="number" value="" size="30" maxlength="100" name="mynumber" id="" /><br /><br />
+   
+	<button id="mysubmit" type="submit">Submit</button><br /><br />
+
+</form>
+</div>
+
+</body>
+</html>
