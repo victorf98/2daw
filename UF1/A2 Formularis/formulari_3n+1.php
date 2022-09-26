3a8ef3fe75040cf0734e4e08ea7862b824de2545
victorf98 <vflores111@boscdelacoma.cat>
2022-09-26 17:47:00 +0200

    Fi A2 Formularis

--- UF1/A2 Formularis/formulari_3n+1.php
+++ UF1/A2 Formularis/formulari_3n+1.php
@@ -11,7 +11,7 @@
 
 <div style="margin: 30px 10%;">
 <h3>3n + 1</h3>
-<form action="processa_3n+1.php" method="post" id="myform" name="myform">
+<form action="" method="post" id="myform" name="myform">
 
 	<label>Numero</label> <input type="number" value="" size="30" maxlength="100" name="mynumber" id="" /><br /><br />
    
@@ -19,6 +19,35 @@
 
 </form>
 </div>
+
+<?php
+        if (array_key_exists("mynumber", $_REQUEST)) {
+            $numero = $_REQUEST["mynumber"];
+            $array[] = $numero;
+            if ($numero < 1) {
+                print_r("El numero ha de ser més gran que 0");
+            }else{
+                while ($numero != 1) {
+                    if ($numero % 2 == 0) {
+                        $numero = $numero / 2;
+                        $array[] = $numero;
+                    }else {
+                        $numero = $numero * 3 + 1;
+                        $array[] = $numero;
+                    }
+                }
+                print_r("La sequüència del " . $array[0] . " és: ");
+                foreach ($array as $key => $value) {
+                    print_r($value . " ");
+                }
+                print_r(". S'ha arraibat a " . count($array) . " iteracons i arribant a un màxim de " . max($array));
+            }
+            }else {
+                die("Introdueix un nombre.");
+            }
+        
+
+    ?>
 
 </body>
 </html>
