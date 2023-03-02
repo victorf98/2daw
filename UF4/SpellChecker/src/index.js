ea9058028438196d675da7848ae1f619d3abe27f
victorf98 <vflores111@boscdelacoma.cat>
2023-03-02 20:03:44 +0100

    començar bot que respongui amb trigger

---
+++ UF4/SpellChecker/src/index.js
@@ -0,0 +1,40 @@
+const { Client, GatewayIntentBits, Partials, ActionRowBuilder, ButtonBuilder } = require('discord.js');
+const { Guilds, GuildMembers, GuildMessages, MessageContent } = GatewayIntentBits;
+const { User, Message, GuildMember, ThreadMember } = Partials;
+
+const client = new Client({
+    intents: [Guilds, GuildMembers, GuildMessages, MessageContent],
+    partials: [User, Message, GuildMember, ThreadMember],
+});
+
+const config = require("../config.json");
+
+client.on("ready", () => {
+    console.log("El cliente ya esta listo");
+});
+
+client.login(config.token);
+
+client.on("messageCreate", async (message) => {
+    if (message.author.bot || !message.guild || message.channel.type === "DM") return;
+    let trigger = config.trigger;
+
+    if (!message.content.split(" ").includes(trigger)) {
+        return;
+    } else {
+        // const embed = new EmbedBuilder()
+        //     .setTitle("Correccion")
+        //     .setDescription("Corrected 📋");
+        
+            const button = new ActionRowBuilder()
+			.addComponents(
+				new ButtonBuilder()
+					.setCustomId('primary')
+					.setLabel('Click me!')
+			);
+
+
+        await message.send({ content: "Corrected 📋" , components: [button]});
+    }
+
+});
